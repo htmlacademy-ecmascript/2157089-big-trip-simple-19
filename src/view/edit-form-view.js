81927292213ac77dc18eaf570599802829319eec
Prokutin <q152827060@yandex.ru>
2023-01-14 20:10:19 +0300

    3.10

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -23,13 +23,13 @@ const renderPicturesForDestination = (pictures) => pictures
 
 const createEditPointTemplate = (point) => {
   const offersForPointType = getOffersForPointType(point);
-  const { basePrice, dateFrom, dateTo } = point;
+  const { basePrice, dateFrom, dateTo, type } = point;
   return `<li class="trip-events__item"><form class="event event--edit" action="#" method="post">
   <header class="event__header">
     <div class="event__type-wrapper">
       <label class="event__type  event__type-btn" for="event-type-toggle-1">
         <span class="visually-hidden">Choose event type</span>
-        <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+        <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
       </label>
       <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
       <div class="event__type-list">
@@ -76,9 +76,9 @@ const createEditPointTemplate = (point) => {
     </div>
     <div class="event__field-group  event__field-group--destination">
       <label class="event__label  event__type-output" for="event-destination-1">
-        Flight
+        ${type}
       </label>
-      <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+      <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${getDestinationForPointId(point).name}" list="destination-list-1">
       <datalist id="destination-list-1">
         <option value="Amsterdam"></option>
         <option value="Geneva"></option>
@@ -136,22 +136,33 @@ const createEditPointTemplate = (point) => {
 };
 
 export default class EditPointView {
+  #element = null;
+  #point = null;
+
   constructor(point) {
-    this.point = point;
+    this.#point = point;
   }
 
-  getTemplate() {
-    return createEditPointTemplate(this.point);
+  get template () {
+    return createEditPointTemplate(this.#point);
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element () {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
-    return this.element;
+    return this.#element;
   }
 
-  removeElement() {
-    this.element = null;
+  get editButton () {
+    return this.element.querySelector('.event__rollup-btn');
+  }
+
+  get submitButton () {
+    return this.element.querySelector('.event--edit');
+  }
+
+  removeElement () {
+    this.#element = null;
   }
 }
